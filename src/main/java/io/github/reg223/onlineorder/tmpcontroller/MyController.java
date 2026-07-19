301138ca1c8929c88dac39c8de6958cb5589365d
reg223 <reg22355@gmail.com>
2026-07-18 22:48:16 -0500

    misc: clean up of old junk

--- src/main/java/io/github/reg223/onlineorder/tmpcontroller/MyController.java
+++ src/main/java/io/github/reg223/onlineorder/tmpcontroller/MyController.java
@@ -1,4 +1,4 @@
-package io.github.reg223.onlineorder.controller;
+package io.github.reg223.onlineorder.tmpcontroller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
